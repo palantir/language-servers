84fce01aa220f12af8508f8eea52f259a94476b0
Jiahui Jiang <jjiang@palantir.com>
2017-10-19 18:25:16 +0100

    Fix an edge case for insertion

--- language-server-commons/src/main/java/com/palantir/ls/util/InMemoryContentsManager.java
+++ language-server-commons/src/main/java/com/palantir/ls/util/InMemoryContentsManager.java
@@ -145,7 +145,8 @@ public final class InMemoryContentsManager implements ContentsManager {
             }
 
             // handle the change under consideration
-            String currentLine = currentContents[lineNum];
+            // if changes are asserted to a new line after the existing contents append it at the end
+            String currentLine = lineNum >= currentContentsLineCount ? "" : currentContents[lineNum];
             contents.append(currentLine.substring(lastColumn, Math.min(start.getCharacter(), currentLine.length())))
                     .append(sortedChanges.get(changeIndex).getText());
 
